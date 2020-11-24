56b1a83f676786f6be3b0708f2bfb4e3e6fd6525
Naitik Gondaliya <33457664+NaitikGondaliya@users.noreply.github.com>
2020-11-24 17:13:44 +0530

    Add files via upload

---
+++ NugetPackageBuilder/Models/APIResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShivOhm.Infrastructure
+{
+    public class ApiResponseModel
+    {
+        public object Data { get; set; }
+        public string Message { get; set; }
+        public object StatusCodes { get; set; }
+    }
+
+    public class PaginationModel<TEntity> where TEntity : class
+    {
+        public int TotalCount { get; set; }
+        public int FilteredCount { get; set; }
+        public IReadOnlyList<TEntity> PageData { get; set; }
+    }
+}
---
+++ NugetPackageBuilder/Models/KeyValueBinder.cs
@@ -0,0 +1,8 @@
+﻿namespace ShivOhm.Infrastructure
+{
+    public class KeyValueControlBinder<IdDataType, NameDataTYpe>
+    {
+        public IdDataType Id { get; set; }
+        public NameDataTYpe Name { get; set; }
+    }
+}

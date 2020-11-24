1f1e5cdb201c2e9199882a5a41276ac39902a276
Naitik Gondaliya <33457664+NaitikGondaliya@users.noreply.github.com>
2020-11-24 17:13:11 +0530

    Add files via upload

---
+++ NugetPackageBuilder/Logging/ExceptionMiddlewareExtension.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace ShivOhm.Infrastructure
+{
+    public static class ExceptionMiddlewareExtension
+    {
+        public static void ConfigureApplication(this IApplicationBuilder app, ILog logger)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeature != null)
+                    {
+                        logger.Error($"{contextFeature.Error}");
+
+                        await context.Response.WriteAsync(new ErrorDetails()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = $"Internal Server Error. Reason: {contextFeature.Error?.Message}"
+                        }.ToString());
+                    }
+                });
+            });
+
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", string.Empty);
+                options.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+                options.DefaultModelsExpandDepth(-1);                
+            });
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ NugetPackageBuilder/Logging/ILog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShivOhm.Infrastructure
+{
+    public interface ILog
+    {
+        void Information(string message);
+        void Warning(string message);
+        void Debug(string message);
+        void Error(string message);
+    }
+}

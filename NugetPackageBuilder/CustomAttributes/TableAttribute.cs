c22b41c24fc2018bb99f80ae8e461fe2978b7dce
Naitik Gondaliya <33457664+NaitikGondaliya@users.noreply.github.com>
2020-11-24 17:06:05 +0530

    Add files via upload

---
+++ NugetPackageBuilder/CustomAttributes/TableAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShivOhm.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public class TableAttribute : Attribute
+    {
+        public string TableName { get; set; }
+    }
+}

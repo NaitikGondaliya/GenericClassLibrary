91b6683ae9e97eb8ca05d9a99f63aa8cb36808e3
Naitik Gondaliya <33457664+NaitikGondaliya@users.noreply.github.com>
2020-11-25 12:39:32 +0530

    Add files via upload

---
+++ NugetPackageBuilder/CustomAttributes/ExcludeColumnAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShivOhm.Infrastructure
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
+    public class ExcludeColumnAttribute : Attribute
+    {
+        public bool AllowAdd { get; set; }
+        public bool AllowUpdate { get; set; }
+        public bool AllowRead { get; set; }
+    }
+}
